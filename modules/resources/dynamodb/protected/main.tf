1b9ec2075e401d9d89be80506c472e34b9dfb5f6
justinon-ss <justin@shoutsense.com>
2017-09-15 20:13:51 -0700

    Added dynamodb base resource

---
+++ modules/resources/dynamodb/protected/main.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "dynamodb-table-template-protected" {
+  name              = "${var.table_name}"
+  read_capacity     = "${var.read_capacity}"
+  write_capacity    = "${var.write_capacity}"
+  hash_key          = "${var.hash_key}"
+  range_key         = "${var.range_key}"
+  stream_enabled    = "${var.stream_enabled}"
+  stream_view_type  = "${var.stream_view_type}"
+  attribute         = "${var.attributes}"
+
+  ttl {
+    attribute_name  = "TimeToExist"
+    enabled         = "${var.enable_ttl}"
+  }
+
+  tags {
+    Environment     = "${var.environment}"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ modules/resources/dynamodb/protected/vars.tf
@@ -0,0 +1,50 @@
+variable region {
+  description = "The region on which to host the s3 buckets"
+}
+
+variable account_id {}
+
+variable role {}
+
+### DYNAMODB VARS ###
+variable table_name {
+  description = "The name for the Dynamo table to be created"
+}
+
+variable read_capacity {
+  description = "The number of read units for this table"
+}
+
+variable write_capacity {
+  description = "The number of write units for this table"
+}
+
+variable hash_key {
+  description = "Attribute to use as the hash key (must be defined as an attribute record)"
+}
+
+variable range_key {
+  description = "Attribute to use as the range key (must be defined as an attribute record)"
+  default     = ""
+}
+
+variable attributes {
+  description = "A list of maps, where each map is a single attribute with name/type"
+  default     = []
+  type        = "list"
+}
+
+variable stream_enabled {
+  default     = false
+}
+
+variable stream_view_type {
+  description = "Set if stream_enabled is true"
+  default     = "NEW_IMAGE"
+}
+
+variable enable_ttl {
+  default     = false
+}
+
+variable environment {}
